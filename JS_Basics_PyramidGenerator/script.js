05e54754c1262498b13af81d3fec536434869084
jsdev3033 <oblokoforme@gmail.com>
2024-04-29 10:04:12 +0300

    code refactoring, removed variables, uodated loops

--- JS_Basics_PyramidGenerator/script.js
+++ JS_Basics_PyramidGenerator/script.js
@@ -2,16 +2,20 @@ const character = "#";
 const count = 8;
 const rows = [];
 
-function padRow(name) {
-  return name;
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
-const call = padRow("CamperChan");
-console.log(call);
+
+// TODO: use a different type of loop
+/*for (let i = 1; i <= count; i++) {
+  rows.push(padRow(i, count));
+}*/
+
+/*while (rows.length < count) {
+  rows.push(padRow(rows.length + 1, count));
+}*/
 
 
-for (let i = 0; i < count; i = i + 1) {
-  rows.push(character.repeat(i + 1))
-}
 
 let result = ""
 
