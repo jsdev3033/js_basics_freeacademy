ed92d3f4bea458e6f4c803977e4b726420380a46
jsdev3033 <oblokoforme@gmail.com>
2024-05-03 13:15:09 +0300

    added new function

--- JS_Basics_Calories_Counter/script.js
+++ JS_Basics_Calories_Counter/script.js
@@ -15,3 +15,8 @@ function isInvalidInput(str) {
   const regex = /\d+e\d+/i;
   return str.match(regex);
 }
+
+function addEntry() {
+  const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
+
+}
